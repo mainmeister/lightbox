1141d71119e4af74b69252c761601acce9f1f0a5
William Main <mainmeister@gmail.com>
2023-01-07 15:05:32 -0500

    make class like

--- average_image.py
+++ average_image.py
@@ -6,6 +6,10 @@ import pyautogui
 import tkinter as tk
 
 
+# from tkinter import *
+# import tkinter
+
+
 def average_color(img):
     avg_color_per_row = np.average(img, axis=0)
     avg_color = np.average(avg_color_per_row, axis=0)
@@ -21,22 +25,36 @@ def average_brightness(img):
         # Grayscale
         return np.average(img)
 
+
 def averages(filename):
     screenshot(filename)
     img = cv2.imread(filename)
     return (average_color(img), average_brightness(img))
+
+
 def screenshot(filename):
     myScreenshot = pyautogui.screenshot()
     myScreenshot.save(filename)
 
 
 if __name__ == "__main__":
+    class myapp:
+
+        def __init__(self, master, filename):
+            self.master = master
+            self.filename = filename
+            self.master.after_idle(self.get_screenshot_and_calculate)
+
+        def get_screenshot_and_calculate(self):
+            color, brightness = averages(self.filename)
+            r, g, b = color
+            self.master['bg'] = '#%02x%02x%02x' % (int(r), int(g), int(b))
+            print(r, g, b, brightness)
+            self.master.update_idletasks()
+            self.master.after(0, self.get_screenshot_and_calculate)
+
+
     screenshot_filename = r'screenshot.png'
-
     root = tk.Tk()
+    app = myapp(root, screenshot_filename)
     root.mainloop()
-
-    while True:
-        screenshot(screenshot_filename)
-        print(averages(screenshot_filename))
-        time.sleep(0.1)
